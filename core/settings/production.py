20340f20efd1be0964efc28f9e26fb0d2871962b
Isaac Fierro <isaaczid@hotmail.com>
2022-10-31 21:38:44 -0700

    Listo para RENDER

--- core/settings/production.py
+++ core/settings/production.py
@@ -1,13 +1,16 @@
 from .base import *
 
 
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
+
 DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 
-if RENDER_EXTERNAL_HOSTNAME: ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+if RENDER_EXTERNAL_HOSTNAME: 
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 DATABASES = {
     'default': dj_database_url.config(
@@ -17,3 +20,7 @@ DATABASES = {
 }
 
 STATIC_URL = 'static/'
+
+if not DEBUG:    
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
