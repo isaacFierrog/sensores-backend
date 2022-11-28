72291f7f9d6aa36a8cc981f67a9cc6fccd4774e2
Isaac Fierro <zifg_36@live.com.mx>
2022-11-28 08:51:37 -0700

    URL servidor agregada

--- templates/static/AuthLayout.2394dbcf.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,r as t,o as r,c,b as n}from"./index.a7903cd4.js";const s={};function a(_,u){const e=t("router-view");return r(),c("div",null,[n(e)])}const f=o(s,[["render",a]]);export{f as default};
---
+++ templates/static/AuthLayout.716b0e16.js
@@ -0,0 +1,1 @@
+import{_ as o,r as t,o as r,c,b as n}from"./index.7ea21ee8.js";const s={};function a(_,u){const e=t("router-view");return r(),c("div",null,[n(e)])}const f=o(s,[["render",a]]);export{f as default};
