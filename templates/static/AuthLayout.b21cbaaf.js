f2ff366604aa261d4cc1a366e9c87e2adc32b27e
Isaac Fierro <zifg_36@live.com.mx>
2022-12-06 11:48:41 -0700

    Cambios agregados

---
+++ templates/static/AuthLayout.b21cbaaf.js
@@ -0,0 +1,1 @@
+import{_ as o,r as t,o as r,c,b as n}from"./index.c99f415b.js";const s={};function a(_,u){const e=t("router-view");return r(),c("div",null,[n(e)])}const f=o(s,[["render",a]]);export{f as default};
